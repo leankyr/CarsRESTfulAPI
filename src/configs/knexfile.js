f5c326b3bd8111f2d598abb7aa299c669557fcad
George Leandros Kyriazis <leankyr@gmail.com>
2022-02-20 22:10:02 +0200

    changed my mind

--- src/configs/knexfile.js
+++ src/configs/knexfile.js
@@ -5,22 +5,22 @@ const path = require('path');
  */
 module.exports = {
 
-  development: {
-    client: 'pg',
-    version: '12.9',
-    connection: {
-      host: process.env.HOST,
-      port: process.env.PORT,
-      user: process.env.USER,
-      password: process.env.PASS,
-      database: process.env.DB
-    },
-    migrations: {
-      directory: path.join(__dirname, '/knex/migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, '/knex/seeds')
-    }
+    development: {
+        client: 'pg',
+        version: '12.9',
+        connection: {
+            host: process.env.HOST,
+            port: process.env.PORT,
+            user: process.env.USER,
+            password: process.env.PASS,
+            database: process.env.DB
+        },
+        migrations: {
+            directory: path.join(__dirname, '/knex/migrations')
+        },
+        seeds: {
+            directory: path.join(__dirname, '/knex/seeds')
+        }
 
-  }
+    }
 };
--- src/configs/schemas.js
+++ src/configs/schemas.js
@@ -1,16 +1,16 @@
 const Joi = require('joi');
 const carSchema = Joi.object().keys({
-  plate: Joi.string().alphanum().min(7).max(7).required(),
-  color: Joi.string().min(2).max(30).required()
+    plate: Joi.string().alphanum().min(7).max(7).required(),
+    color: Joi.string().min(2).max(30).required()
 });
 
 const driverSchema = Joi.object().keys({
-  first_name: Joi.string().alphanum().min(3).max(30).required(),
-  last_name: Joi.string().alphanum().min(3).max(30).required(),
-  car_id: Joi.number().integer().min(0)
+    first_name: Joi.string().alphanum().min(3).max(30).required(),
+    last_name: Joi.string().alphanum().min(3).max(30).required(),
+    car_id: Joi.number().integer().min(0)
 });
 
 module.exports = {
-  carSchema,
-  driverSchema
+    carSchema,
+    driverSchema
 };
