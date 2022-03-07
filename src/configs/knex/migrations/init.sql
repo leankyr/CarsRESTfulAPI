8d917d32da1eb1db269a12f2ddcc3a8fe1287c16
George Leandros Kyriazis <leankyr@gmail.com>
2022-03-07 20:02:47 +0200

    playing with docker compose and Dockerfile

--- src/configs/knex/migrations/init.sql
+++ src/configs/knex/migrations/init.sql
@@ -1,21 +1,19 @@
 CREATE TABLE IF NOT EXISTS public.cars
 (
-    car_id integer NOT NULL DEFAULT nextval('cars_car_id_seq'::regclass),
+    car_id serial PRIMARY KEY NOT NULL,
     plate character varying(50) COLLATE pg_catalog."default" NOT NULL,
     color character varying(20) COLLATE pg_catalog."default" NOT NULL,
     created_on timestamp without time zone NOT NULL,
-    CONSTRAINT cars_pkey PRIMARY KEY (car_id),
     CONSTRAINT cars_plate_key UNIQUE (plate)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS public.drivers
 (
-    driver_id integer NOT NULL DEFAULT nextval('drivers_driver_id_seq'::regclass),
+    driver_id serial PRIMARY KEY NOT NULL,
     first_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
     last_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
     created_on timestamp without time zone NOT NULL,
     car_id integer,
-    CONSTRAINT drivers_pkey PRIMARY KEY (driver_id),
     CONSTRAINT drivers_car_id_fkey FOREIGN KEY (car_id)
     REFERENCES public.cars (car_id) MATCH SIMPLE
                          ON UPDATE NO ACTION
