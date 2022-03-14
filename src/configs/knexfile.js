df31f929b16c716c6fa8780a5514bdc1f27ed01f
George Leandros Kyriazis <leankyr@gmail.com>
2022-03-14 14:00:45 +0200

    fixed db for gcloud

--- src/configs/knexfile.js
+++ src/configs/knexfile.js
@@ -1,19 +1,24 @@
 // Update with your config settings.
 const path = require('path');
 /**
- * @type { Object.<string, import("knex").Knex.Config> }
+ * @type {string|string}
  */
+
+const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+
 module.exports = {
 
     development: {
         client: 'pg',
-        version: '12.9',
+        version: '12.8',
         connection: {
-            host: process.env.HOST,
-            port: process.env.PORT,
+            // host: process.env.HOST,
+            // port: process.env.DB_PORT,
             user: process.env.USER,
             password: process.env.PASS,
-            database: process.env.DB
+            database: process.env.DB,
+            host: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`,
+
         },
         migrations: {
             directory: path.join(__dirname, '/knex/migrations')
