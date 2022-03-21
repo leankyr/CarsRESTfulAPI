bfd93a6489ecbdaf4be8b00e8d92da77b913a8a4
George Leandros Kyriazis <leankyr@gmail.com>
2022-03-21 18:26:12 +0200

    completed tests

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 const server = require('../src/app');
 const should = chai.should();
 const car_repo = require('../src/repositories/car_repository')
-
+const driver_repo = require('../src/repositories/driver_repository')
 
 chai.use(chaiHttp);
 
@@ -59,6 +59,20 @@ describe('/POST cars', () => {
                 res.body.should.be.a('object');
                 res.body.should.have.property('msg');
                 res.body.should.have.property('msg').eql("car with this plate already exists!");
+                done();
+            });
+    });
+
+    it('it schema is wrong it should return 400', (done) => {
+        const car = {
+            plate: 'tes0001',
+            whatever:  'test'
+        }
+        chai.request(process.env.SERVER)
+            .post('/cars')
+            .send(car)
+            .end((err, res) => {
+                res.should.have.status(400);
                 done();
             });
     });
@@ -156,7 +170,7 @@ describe('/GET Drivers', () => {
 });
 
 describe('/POST Drivers', () => {
-    it('it should return status 201',  async(done) => {
+    it('it should return status 201',  async() => {
         const car = {
             plate: 'drv0000',
             color: 'testDriver'
@@ -178,8 +192,117 @@ describe('/POST Drivers', () => {
                 res.body.should.be.a('object');
                 res.body.should.have.property('success');
                 res.body.should.have.property('data');
-                done();
+            });
+    });
+
+    it('it should return status 400 when the schema is wrong',  async() => {
+        const car = {
+            plate: 'drv0001',
+            color: 'testDriver'
+        }
+        // is that right?
+        const car_id = await car_repo.postCars(car);
+        // weird thing happened in this test.
+        // It passed the test even though it should not (change driver last name to color)
+        const driver = {
+            first_name: 'Tilemachos',
+            color:  'Kyriazis',
+            car_id: car_id[0]["car_id"]
+        };
+        chai.request(process.env.SERVER)
+            .post('/drivers')
+            .send(driver)
+            .end((err, res) => {
+                res.should.have.status(400);
+            });
+    });
+
+});
+
+describe('/GET Driver by ID', () => {
+    it('get driver by id should return status 200',  async() => {
+        const car = {
+            plate: 'drv0002',
+            color: 'testDriver'
+        }
+        // is that right?
+        const car_id = await car_repo.postCars(car);
+        console.log(car_id[0]["car_id"])
+        // weird thing happened in this test.
+        // It passed the test even though it should not (change driver last name to color)
+        const driver = {
+            first_name: 'John',
+            last_name:  'Doe',
+            car_id: car_id[0]["car_id"]
+        };
+        const driver_id = await driver_repo.postDrivers(driver);
+        chai.request(process.env.SERVER)
+            .get('/drivers/' + driver_id[0]['driver_id'])
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('driver');
             });
     });
 });
+
+describe('/PUT Driver by ID', () => {
+    it('it should return status 200',  async() => {
+        const car = {
+            plate: 'drv0003',
+            color: 'testDriver'
+        }
+        // is that right?
+        const car_id = await car_repo.postCars(car);
+        // weird thing happened in this test.
+        // It passed the test even though it should not (change driver last name to color)
+        const driver = {
+            first_name: 'Test',
+            last_name:  'Driver',
+            car_id: car_id[0]["car_id"]
+        };
+        const driver_id = await driver_repo.postDrivers(driver);
+        chai.request(process.env.SERVER)
+            .put('/drivers/' + driver_id[0]['driver_id'])
+            .send({
+                first_name: 'Updated',
+                last_name:  'Driver',
+                car_id: car_id[0]["car_id"]
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('success');
+                // res.body.should.have.property('driver_id').eql(driver_id[0]['driver_id']);
+            });
+    });
+});
+
+describe('/DELETE driver by ID', () => {
+    it('it should delete a driver by the given ID', async() => {
+        const car = {
+            plate: 'del0002',
+            color: 'del'
+        }
+        // is that right?
+        const car_id = await car_repo.postCars(car);
+        const driver = {
+            first_name: 'Deleted',
+            last_name:  'Driver',
+            car_id: car_id[0]["car_id"]
+        };
+        const driver_id = await driver_repo.postDrivers(driver);
+        chai.request(process.env.SERVER)
+            .delete('/drivers/' + driver_id[0]["driver_id"])
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('msg').eql(
+                    "Driver with id=" + driver_id[0]["driver_id"] + " deleted successfully");
+                res.body.should.have.property('data');
+                // res.body.should.have.property('car_id').eql(car_id[0]["car_id"]);
+            });
+    });
+});
+
 
